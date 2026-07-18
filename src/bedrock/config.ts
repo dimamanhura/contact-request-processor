abbec96554456f269e9aafbf45411bb46dc79a8c
Manhura Dima <manguradima@gmail.com>
2026-07-18 19:54:48 +0300

    New arn for haiky model

--- src/bedrock/config.ts
+++ src/bedrock/config.ts
@@ -1,6 +1,6 @@
 const config = {
   REGION: "us-east-1",
-  MODEL_ID: "anthropic.claude-haiku-4-5-20251001-v1:0",
+  MODEL_ID: "us.anthropic.claude-haiku-4-5-20251001-v1:0",
   ANTHROPIC_VERSION: "bedrock-2023-05-31",
   MAX_TOKENS: 200,
   TEMPERATURE: 0.1,
